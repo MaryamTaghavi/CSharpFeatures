ee300915d6ce6c4c04e821ec5a7f561e116eb3ba
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-07-31 11:41:06 +0330

    Add"d"Format

--- 03_FormattingConsoleOutput/Program.cs
+++ 03_FormattingConsoleOutput/Program.cs
@@ -4,3 +4,7 @@ Console.WriteLine("{1}, {0}, {2}" , 10 , 20 , 30) ;
 // C or c : used to format currency, by default the flag will prefix the local cultural symbol.
 // c format: $99,999.00
 Console.WriteLine("c format: {0:c}" , 99999) ;
+
+// D or d : used to format decimal numbers. This flag may also specify minimum number of digits used to pad the value.
+// d9 format: 000099999
+Console.WriteLine("d9 format: {0:d9}" , 99999) ;
