46135a123e8b4e8721ae6fff75f19409802ac6e4
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-07-31 11:36:15 +0330

    Add"C"Format

--- 03_FormattingConsoleOutput/Program.cs
+++ 03_FormattingConsoleOutput/Program.cs
@@ -1,1 +1,6 @@
-03
+// Prints : 20, 10, 30
+Console.WriteLine("{1}, {0}, {2}" , 10 , 20 , 30) ;
+
+// C or c : used to format currency, by default the flag will prefix the local cultural symbol.
+// c format: $99,999.00
+Console.WriteLine("c format: {0:c}" , 99999) ;
