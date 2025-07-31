ec70884f53f8328c6d95e3060f9cd129c1009e8d
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-07-31 11:14:16 +0330

    Update Program.cs

--- 02_GlobalUsingStatements/Program.cs
+++ 02_GlobalUsingStatements/Program.cs
@@ -1,1 +1,5 @@
+// In C# 10 , you can use Global Using Statements when you want to use one using in several files.
+// Usually it can be in Program.cs 
 
+global using System;
+global using System.Collections.Generic;
