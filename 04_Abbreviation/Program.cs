adbea0c7684f958dd937af87472d01630500969f
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-02 09:53:28 +0330

    Update Program.cs

--- 04_Abbreviation/Program.cs
+++ 04_Abbreviation/Program.cs
@@ -1,1 +1,15 @@
+// CLS => Common Language Specification Compliant (سازگاری با قواعد حداقلی .Net)
 
+// CLI => Command Line Interface (رابط خط فرمان)
+
+// GUI => Graphical User Interface (رابط گرافیکی)
+
+// LTS => Long Term Support (پشتیبانی طولانی مدت)
+
+// LINQ => Language Integrated Query (کوئری نویسی یکپارچه با زبان برنامه نویسی)
+
+// CIL => Common Intermediate Language (زبانی که کدهای .net و ... بعد از کامپایل شدن و قبل از اجرا به آن تبدیل میشوند.)
+
+// IDE => Integrated Development Environments ( محیط توسعه نرم افزار مثل visual stadio)
+
+// CTS => Common Type System (S توی دات‌نت مشخص می‌کنه که چه نوع داده‌هایی (data types) توی همه زبان‌های دات‌نت مثل C#, VB.NET, F# و ... وجود دارن و چطوری با هم سازگارن.)
