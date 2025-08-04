c298767c98d12acab02ae9f485b9f06cc34c0db6
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-04 15:22:58 +0330

    Update Program.cs

--- 07_StringInterpolation/Program.cs
+++ 07_StringInterpolation/Program.cs
@@ -1,3 +1,6 @@
+int age = 4 ;
+string name = "Maryam" ;
+
 // Using curly-bracket syntax
 string greeting = String.Format("Hello {0} you are {1} years old." , name , age) ;
 Console.WriteLine(greeting) ;
