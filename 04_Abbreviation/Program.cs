97eb421f6d6e746f77527852a183ff5eeb294ff1
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-04 08:10:40 +0330

    Update Program.cs

--- 04_Abbreviation/Program.cs
+++ 04_Abbreviation/Program.cs
@@ -14,4 +14,4 @@
 
 // CTS => Common Type System (S توی دات‌نت مشخص می‌کنه که چه نوع داده‌هایی (data types) توی همه زبان‌های دات‌نت مثل C#, VB.NET, F# و ... وجود دارن و چطوری با هم سازگارن.)
 
-//SDK => Software Development Kit (ابزارهای توسعه نرم افزار)
+// SDK => Software Development Kit (ابزارهای توسعه نرم افزار)
