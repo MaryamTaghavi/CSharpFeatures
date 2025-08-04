98ee9b7c1b35e2f3dec3971046ba0e8f3224b677
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-04 15:22:03 +0330

    Create Program.cs

---
+++ 07_StringInterpolation/Program.cs
@@ -0,0 +1,7 @@
+// Using curly-bracket syntax
+string greeting = String.Format("Hello {0} you are {1} years old." , name , age) ;
+Console.WriteLine(greeting) ;
+
+//Using string interpolation
+string greeting2 = $"Hello {name} you are {age} years old." ;
+Console.WriteLine(greeting2) ;
