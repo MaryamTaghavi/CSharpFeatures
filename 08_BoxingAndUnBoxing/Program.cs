7463740892cacc60521d9832cfe440d25e486766
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-04 22:50:58 +0330

    Update Program.cs

--- 08_BoxingAndUnBoxing/Program.cs
+++ 08_BoxingAndUnBoxing/Program.cs
@@ -1,1 +1,10 @@
+Boxing => Convert ValueType To ReferenceType
+int a = 4 ;
+object b = a ;
 
+UnBoxing => Convert ReferenceType that boxed before To ValueType
+object e = 4 ;
+int x = (int) e ;
+
+// boxing همیشه خودکار انجام میشود ولی unboxing حتما باید cast شود.
+// boxing همیشه پرهزینه است.
