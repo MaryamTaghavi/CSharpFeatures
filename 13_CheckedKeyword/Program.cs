3ff3b200f42c949fff961d01a261fee185939338
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-05 13:31:57 +0330

    UnCheckedKeyword.cs

--- 13_CheckedKeyword/Program.cs
+++ 13_CheckedKeyword/Program.cs
@@ -11,3 +11,5 @@ catch
 }
 
 // If we write checked keyword, if result overflow we force C# to get an error to us.
+// The default behavior of the .Net core runtime is to ignore overflow/underflow.
+// The unchecked keyword can be used if you have a block of code where overflow is acceptable.
