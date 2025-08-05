2ace2e5a85836fea288328af5f6d847c21ceca9b
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-05 12:10:22 +0330

    DataTypeConversions

--- NarrowingWideningDataTypeConversions/Program.cs
+++ NarrowingWideningDataTypeConversions/Program.cs
@@ -1,1 +1,13 @@
-N
+// Narrowing DataType Conversions
+// Convert big type to small type 
+// It's danger and must be case beacause you may miss the value
+
+int b = 30000 ;
+short a = (short) b ; 
+
+// Widening DataType Conversions
+// Convert small type to big type
+// It's ok and it isn't danger
+
+short a = 2 ;
+int b = a ;
