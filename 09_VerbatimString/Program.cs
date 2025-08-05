437ca4edd9fa2334765023757a6f6fe694216de6
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-05 04:05:10 +0330

    Update Program.cs

--- 09_VerbatimString/Program.cs
+++ 09_VerbatimString/Program.cs
@@ -1,1 +1,6 @@
+// When you prefix a string with the @symbol you have created what is termed a "verbatim string", insted of \\ (escape characters) we can use \
 
+Console.WriteLine(@"C:\MyApp\bin\Debug");
+
+// In C#8 and after $@ or @$ will work.
+// فاصله ها (whitespace) و line breaks دقیقا حفظ میشوند.
