a5d91baff39fa85cec393e8689c0e6ff707c345b
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-05 12:56:48 +0330

    Update Program.cs

--- CheckedKeyword/Program.cs
+++ CheckedKeyword/Program.cs
@@ -1,1 +1,13 @@
+try
+{
+  checked 
+  {
+    byte sum = (byte)Add(b1,b2) ;
+    Console.WriteLine("sum = {0}" , sum) ;
+  }
+}
+catch
+{
+}
 
+// If we write checked keyword, if result overflow we force C# to get an error to us.
