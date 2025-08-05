5f48a00b9725bf26d1d883c62edab594da44f12c
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-05 14:27:05 +0330

    Update Program.cs

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,1 +1,5 @@
+// If we use var object and in runtime we want to know about the type of a variable we can use **Reflection**
 
+var myInt = 0 ;
+myInt.GetType().Name 
+// Reflection can defines at **RunTime** 
