fa77195fc39925ecd90e49f56b247197140f2cb5
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-05 04:43:52 +0330

    stringComparisonMethods

--- 10_StringComparison/Program.cs
+++ 10_StringComparison/Program.cs
@@ -1,1 +1,13 @@
+// C# is case sensitive and functions like Comare() , Equals() , == , IndexOf() are case sensitive
+// Use ToUpper() , ToLower() make a copy of string and this is not good for performance.
 
+if( firstString.ToUpper() == secondString.ToUpper() )
+{
+    //Do something
+}
+
+// All functions for compare have "StringComparison"
+if (string.Equals(firstString, secondString, StringComparison.OrdinalIgnoreCase))
+{
+    // Do something
+}
