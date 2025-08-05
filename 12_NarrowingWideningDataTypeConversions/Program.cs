7e7150683760094e427b02914405806c4c632ed4
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-05 12:27:43 +0330

    Update Program.cs

--- 12_NarrowingWideningDataTypeConversions/Program.cs
+++ 12_NarrowingWideningDataTypeConversions/Program.cs
@@ -1,13 +1,14 @@
 // Narrowing DataType Conversions
 // Convert big type to small type 
 // It's danger and must be case beacause you may miss the value
-
+** Explicit casting **
 int b = 30000 ;
 short a = (short) b ; 
 
 // Widening DataType Conversions
 // Convert small type to big type
 // It's ok and it isn't danger
+** Implicit casting **
 
 short a = 2 ;
 int b = a ;
