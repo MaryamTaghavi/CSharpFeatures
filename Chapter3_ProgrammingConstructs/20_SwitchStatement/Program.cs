7a73f421f48d2c8b6da9eeb633d709e7e80ce97e
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-07 15:30:28 +0330

    switchCaseStatement

--- Chapter3_ProgrammingConstructs/20_SwitchStatement/Program.cs
+++ Chapter3_ProgrammingConstructs/20_SwitchStatement/Program.cs
@@ -1,1 +1,21 @@
+// Before C#8
+var colorBand ;
+switch (colorBand)
+{
+  case "Red" :
+    return "#FF0000" ;
+  case "Blue" :
+    return "#0000FF" ;
+  default :
+    return "#FFFFFF" ;
+}
 
+// It's new in C# 8
+return colorBand switch
+{
+    "Red" => "#FF0000",
+    "Orange" => "#FF7F00",
+    _ => "#FFFFFF"
+} ;
+
+// We can pass into switch **Tuple** (first,second)
