6ad833ab5bd85625c5ac54de50e606b3970e96d5
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-07 12:14:29 +0330

    Update Program.cs

--- 15_Iteration Constructs/Program.cs
+++ 15_Iteration Constructs/Program.cs
@@ -1,1 +1,11 @@
+// for loop 
+// foreach / in loop
 
+// Differences between for and fooreach :
+// 1) for use index and foreach don't use
+// 2) foreach often use for readonly and for use change data
+
+// while
+// do-while
+
+// With while we may don't see the result but in do-while we see at least on result.
