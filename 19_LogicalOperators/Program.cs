3ede4bcba3f064d628ede24da5d748193cdfcc2a
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-08-07 14:17:18 +0330

    describeLogicalExpression

--- 19_LogicalOperators/Program.cs
+++ 19_LogicalOperators/Program.cs
@@ -1,1 +1,10 @@
+// || &&
+// | &
+// if (A() && B()) => if A() == false B() doesn't check
+// if (A() & B()) => even if A() == false B() check
+// if (A() || B()) => if A() == true B() doesn't check
+// if (A() | B()) => even if A() == true B() check
 
+
+// The && and || operators both "short-circuit" when necessory. This means that after a complex expression has been determined to be false,
+// the remaining subexpressions will not be checked. If you require all expressions to be tested regardless, you can use the related & and | operators.
