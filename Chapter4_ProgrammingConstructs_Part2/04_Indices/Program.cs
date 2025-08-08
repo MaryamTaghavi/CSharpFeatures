a63ba299092bd1e65bee9cbe570d73b9e859cfeb
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-08 20:12:31 +0330

    Indices

---
+++ Chapter4_ProgrammingConstructs_Part2/04_Indices/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] gothicBands = { "Tones On Tail" , "Bauhaus" , "Sisters of Mercy"};
+
+for (int i = 1; i <= gothicBands.Length; i++)
+{
+    Index inx = ^i;
+    Console.WriteLine(gothicBands[inx] + ", ");
+}
