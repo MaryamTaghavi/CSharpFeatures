be8f8aa94c0c4862dc6b019a4040c84ae4c4cce7
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-08 20:03:55 +0330

    jaggedArray

---
+++ Chapter4_ProgrammingConstructs_Part2/03_JaggedArrays/Program.cs
@@ -0,0 +1,16 @@
+﻿int[][] myJaggArray = new int[5][];
+
+for (int i = 0; i < myJaggArray.Length; i++)
+{
+    myJaggArray[i] =new int[i+7];
+}
+
+for (int i = 0;i < 5;i++)
+{
+    for(int j = 0; j < myJaggArray[i].Length; j++)
+    {
+        Console.WriteLine(myJaggArray[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
