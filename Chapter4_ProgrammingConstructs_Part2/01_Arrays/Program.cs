9cdf314c025d459c7e262d9a4670895dfe17d618
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-08 19:55:32 +0330

    RectangularArray

--- Chapter4_ProgrammingConstructs_Part2/01_Arrays/Program.cs
+++ Chapter4_ProgrammingConstructs_Part2/01_Arrays/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿object[] myObjects = new object[4];
+
+myObjects[0] = 10;
+myObjects[1] = false;
+myObjects[2] = "SeriLog";
+myObjects[3] = new DateTime(1969,3,24);
+
+foreach(var obj in myObjects)
+{
+    Console.WriteLine("Type: {0}, Value: {1}" , obj.GetType() , obj);
+}
+
+Console.WriteLine();
---
+++ Chapter4_ProgrammingConstructs_Part2/02_RectangularArray/Program.cs
@@ -0,0 +1,23 @@
+﻿int[,] myMatrix;
+
+myMatrix = new int[3, 4];
+
+for(int i = 0; i <3; i++)
+{
+    for(int j = 0; j < 4; j++)
+    {
+        myMatrix[i, j] = i * j;
+    }
+}
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.WriteLine(myMatrix[i, j] + "\t");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
