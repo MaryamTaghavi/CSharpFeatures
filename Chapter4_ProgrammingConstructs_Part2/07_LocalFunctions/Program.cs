3f7fdd44a2b46d3b9e8b4578d116f201fbe70d2a
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-09 16:03:22 +0330

    changeLocalFunction

--- Chapter4_ProgrammingConstructs_Part2/07_LocalFunctions/Program.cs
+++ Chapter4_ProgrammingConstructs_Part2/07_LocalFunctions/Program.cs
@@ -1,7 +1,20 @@
 ﻿// Local functions means a function defines into another function
-private static voidn Process(string?[] lines , string mark)
+
+using System.Diagnostics.CodeAnalysis;
+
+
+string?[] lines = { "apple", null, "banana" };
+Process(lines, "a");
+
+var result = AddWrapperWithSideEffect(10 , 7);
+
+var result2 = AddWrapperWithoutSideEffect(7 , 4);
+
+Console.WriteLine(result);
+
+static void Process(string?[] lines, string mark)
 {
-    foreach(var line in lines)
+    foreach (var line in lines)
     {
         if (IsValid(line))
         {
@@ -14,3 +27,31 @@ private static voidn Process(string?[] lines , string mark)
         return !string.IsNullOrEmpty(line) && line.Length >= mark.Length;
     }
 }
+
+// New in C# 8 Local function can change the values of the variables.
+// Local function can change the variable from the manin function directly.
+
+static int AddWrapperWithSideEffect(int x, int y)
+{
+    return Add();
+
+    int Add()
+    {
+        x += 1;
+        return x + y;
+    }
+}
+
+// If we want the local function cann't change the variables we must change that to static
+
+static int AddWrapperWithoutSideEffect(int x, int y)
+{
+    return Add(x , y);
+
+    static int Add(int x , int y)
+    {
+        x += 1;
+        return x + y;
+    }
+}
+
