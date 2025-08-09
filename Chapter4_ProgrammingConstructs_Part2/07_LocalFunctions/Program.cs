c02d016110cbf4bdb291f41ea1be87fb3461a7d5
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-09 15:20:46 +0330

    addLocalFunctions

---
+++ Chapter4_ProgrammingConstructs_Part2/07_LocalFunctions/Program.cs
@@ -0,0 +1,16 @@
+﻿// Local functions means a function defines into another function
+private static voidn Process(string?[] lines , string mark)
+{
+    foreach(var line in lines)
+    {
+        if (IsValid(line))
+        {
+            // Processing logic...
+        }
+    }
+
+    bool IsValid([NotNullWhen(true)] string? line)
+    {
+        return !string.IsNullOrEmpty(line) && line.Length >= mark.Length;
+    }
+}
