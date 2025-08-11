acab54b2ce409fd7578493525681301625e87639
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-11 10:38:17 +0330

    addEnumValue

---
+++ Chapter4_ProgrammingConstructs_Part2/11_EnumValue/Program.cs
@@ -0,0 +1,15 @@
+﻿
+var result = Enum.GetUnderlyingType(typeof(EmpTypeEnum));
+
+Console.WriteLine(result);
+Console.ReadLine();
+
+enum EmpTypeEnum
+{
+    first = 1,
+    second = 2,
+    third = 3,
+    fourth = 4,
+    fifth = 5,
+    sixth = 6,
+}
