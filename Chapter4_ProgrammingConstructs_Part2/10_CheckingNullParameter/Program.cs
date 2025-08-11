673f3c59981aba485cabdd90f04042883d3b2805
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-11 10:13:00 +0330

    checjingNullParaneter

---
+++ Chapter4_ProgrammingConstructs_Part2/10_CheckingNullParameter/Program.cs
@@ -0,0 +1,8 @@
+﻿
+EnterLogData("has bug");
+
+static void EnterLogData(string message , string owner = "Programmer")
+{
+    ArgumentNullException.ThrowIfNull(message);
+    Console.WriteLine("Error : {0}" , message ) ;
+}
