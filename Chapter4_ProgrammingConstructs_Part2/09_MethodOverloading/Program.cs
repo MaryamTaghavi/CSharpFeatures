b1d7d35472f0637d2999e23859c90ee9d3dc57fb
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-11 10:03:20 +0330

    addMethodOverloading

---
+++ Chapter4_ProgrammingConstructs_Part2/09_MethodOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine(AddOperations.Add(10, 10));
+
+Console.WriteLine(AddOperations.Add(10.4, 17.6));
+
+Console.ReadLine();
+
+public static class AddOperations
+{
+    // Overload Add() method.
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    public static long Long(long x, long y)
+    {
+        return x + y;
+    }
+}
+
