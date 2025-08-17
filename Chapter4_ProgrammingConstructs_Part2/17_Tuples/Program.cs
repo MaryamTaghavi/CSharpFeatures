889c0137f6e2510281f8c6290cb06c401fa51e20
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-17 12:32:18 +0330

    Equality/InequalityInTuples

--- Chapter4_ProgrammingConstructs_Part2/17_Tuples/Program.cs
+++ Chapter4_ProgrammingConstructs_Part2/17_Tuples/Program.cs
@@ -10,3 +10,19 @@ Console.WriteLine(values1.First);
 
 Console.WriteLine(" => Nested Tuples");
 var nt = (5, 4, ("a", "b"));
+
+
+// Equality/Inequality
+
+Console.WriteLine("=> Tuples Equality/Inequality");
+// lifted conversions
+var left = (a: 5, b: 10);
+(int? a, int? b) nullableMembers = (5, 10);
+Console.WriteLine(left == nullableMembers); // Also true
+// converted type of left is (long, long)
+(long a, long b) longTuple = (5, 10);
+Console.WriteLine(left == longTuple); // Also true
+// comparisons performed on (long, long) tuples
+(long a, int b) longFirst = (5, 10);
+(int a, long b) longSecond = (5, 10);
+Console.WriteLine(longFirst == longSecond); // Also true
