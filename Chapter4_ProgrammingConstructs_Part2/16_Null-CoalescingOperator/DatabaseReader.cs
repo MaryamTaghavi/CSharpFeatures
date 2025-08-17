c599d94708a78a3643da3c70e800e34fd5bc791a
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-17 11:48:58 +0330

    addNullCoalescingOperator

---
+++ Chapter4_ProgrammingConstructs_Part2/16_Null-CoalescingOperator/DatabaseReader.cs
@@ -0,0 +1,14 @@
+﻿namespace _16_Null_CoalescingOperator;
+
+class DatabaseReader
+{
+    // Nullable data field.
+    public int? numericValue = null;
+    public bool? boolValue = true;
+    // Note the nullable return type.
+    public int? GetIntFromDatabase()
+    { return numericValue; }
+    // Note the nullable return type.
+    public bool? GetBoolFromDatabase()
+    { return boolValue; }
+}
---
+++ Chapter4_ProgrammingConstructs_Part2/16_Null-CoalescingOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using _16_Null_CoalescingOperator;
+
+Console.WriteLine("***** Fun with Nullable Value Types *****\n");
+DatabaseReader dr = new DatabaseReader();
+// If the value from GetIntFromDatabase() is null,
+// assign local variable to 100.
+int myData = dr.GetIntFromDatabase() ?? 100;
+Console.WriteLine("Value of myData: {0}", myData);
+
+//Null-coalescing assignment operator
+int? nullableInt = null;
+nullableInt ??= 12;
+nullableInt ??= 14;
+Console.WriteLine(nullableInt);
+
+Console.ReadLine();
