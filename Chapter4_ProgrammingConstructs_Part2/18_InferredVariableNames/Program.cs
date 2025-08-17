411f13d0bc4b908d51bd24894be6a3ee5d9b31a0
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-17 12:31:08 +0330

    addInferredVariableNames

---
+++ Chapter4_ProgrammingConstructs_Part2/18_InferredVariableNames/Program.cs
@@ -0,0 +1,7 @@
+﻿// یعنی کامپایلر خودش از اسم متغیر یا مقدارش نوع آن را حدس بزند
+
+Console.WriteLine("=> Inferred Tuple Names");
+var foo = new { Prop1 = "first", Prop2 = "second" };
+var bar = (foo.Prop1, foo.Prop2);
+
+Console.WriteLine($"{bar.Prop1};{bar.Prop2}");
