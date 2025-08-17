a93848c2ad56d67a1c33d1dbd9ea997544fd1c86
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-17 12:19:59 +0330

    createTuples

---
+++ Chapter4_ProgrammingConstructs_Part2/17_Tuples/Program.cs
@@ -0,0 +1,12 @@
+﻿// Define Tuple
+
+(string First, int Second, string Third) values1 = ("a", 5, "c");
+
+var values2 = (First: "a", Second: 5, Third: "c");
+
+Console.WriteLine(values1.Item1);
+
+Console.WriteLine(values1.First);
+
+Console.WriteLine(" => Nested Tuples");
+var nt = (5, 4, ("a", "b"));
