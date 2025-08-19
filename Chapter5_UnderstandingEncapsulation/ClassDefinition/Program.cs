0527f822470465614a70a46b87cca1db292561e7
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-19 16:18:08 +0330

    defineClass

---
+++ Chapter5_UnderstandingEncapsulation/ClassDefinition/Program.cs
@@ -0,0 +1,7 @@
+﻿// تعریف کلاس : یک user-defined type است که مجموعه ای از
+// فیلدها که member variables هم گفته میشوند را نگه میدارد
+// علاوه بر آن رفتارهای آنها شامل methods, events, constructors, properties را هم نگه میدارد
+// که مجموعه اینها تحت عنوان کلاس (class) یا آبجکت (object) نامیده میشوند.
+// اگر یک کلاس را با keyword new ایجاد کنیم و یک نمونه (instance) در آن ایجاد کنیم به آن آبجکت میگویند
+// object allocated into memory using the new keyword.
+
