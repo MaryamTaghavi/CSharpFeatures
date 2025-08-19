fb5f7e4ed1c0b9761f803ec7ed41b2bba87480a7
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-19 16:41:10 +0330

    createConstructor

---
+++ Chapter5_UnderstandingEncapsulation/Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Car
+{
+    public string petName;
+    public int currSpeed;
+
+    // Constructors as expression-bodied members
+    public Car(string pn) => petName = pn;
+
+    // Constructors with out parameters
+    public Car(string pn, int cs, out bool inDanger)
+    {
+        petName = pn;
+        currSpeed = cs;
+        if (cs > 100)
+        {
+            inDanger = true;
+        }
+        else
+        {
+            inDanger = false;
+        }
+    }
+}
