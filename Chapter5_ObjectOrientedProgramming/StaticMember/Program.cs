b3c0481f0f388efb047e3aecab7c56ea35e3e6f7
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-20 15:22:05 +0330

    addStaticClass

--- Chapter5_ObjectOrientedProgramming/StaticMember/Program.cs
+++ Chapter5_ObjectOrientedProgramming/StaticMember/Program.cs
@@ -38,6 +38,13 @@ class SavingsAccount1
 // 3) static constructor
 // برای مواقعی که نیاز به محاسبه در لحظه runtime یا گرفتن دیتا از منابع بیرونی هستیم
 
+/* ویژگی های static constructor :
+ * 1) هر کلاس حتما یک static constructor دارد به عبارت دیگر static constructor نمیتواند overload شو
+ * 2) نمیتواند access modifier و پارامتر ورودی داشته باشد.
+ * 3)  فقط یک بار ایجاد میشود، هر چند بار که بخواهیم کلاس را ایجاد کنیم.
+ * 4) در زمان runtime اجرا میشود قبل از صدا زدن static member ها.
+ */
+
 class Bank
 {
     public static double InterestRate;
@@ -54,8 +61,31 @@ class Bank
         Console.WriteLine("Interest Rate = " + InterestRate);
     }
 }
- 
+
 // نکته : اگر فیلد استاتیک را درون non-static constructor بگیریم با هر بار ایجاد شی این مقدار ریست شده و مقدار تکراری میگیرد
 
+// 4) static class
+// هیچ دفعه allocate in heap در آن اتفاق نمی افتد
+// به آن utility class میگویند
+// حتما باید تمامی اعضای آن static باشند
+// نمیتوان آن را new کرد
 
+static class Bank1
+{
+    public static double InterestRate;
+
+    //compiler error
+    //void Bank1()
+    static Bank1()
+    {
+        // مثلا مقدار رو از فایل config یا یک منبع بیرونی بخونیم
+        Console.WriteLine("Static constructor executed!");
+        InterestRate = DateTime.Now.Second % 2 == 0 ? 0.04 : 0.05;
+    }
+
+    public static void ShowRate()
+    {
+        Console.WriteLine("Interest Rate = " + InterestRate);
+    }
+}
 
