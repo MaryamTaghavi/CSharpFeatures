0692020cec604d2faa28c895fc30bda79f6b913b
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-22 10:32:32 +0330

    createAccessModifier

---
+++ Chapter5_ObjectOrientedProgramming/AccessModifiers/Program.cs
@@ -0,0 +1,18 @@
+﻿// public => access from other assembly
+
+// private => only by class that defines the item
+
+// protected => by the class that defines it , any child class. => in the inheritance chain ( with the different assembly)
+
+// internal => in the current assembly
+
+// protected internal => 2 different state 1)in the same assembly 2)external assembly when they are derived class and in the inheritance chain
+
+// private protected =>  by the class that defines it , any child class (derived class) and same assembly.
+
+
+
+
+
+
+// by default => class is internal and constructor is private
