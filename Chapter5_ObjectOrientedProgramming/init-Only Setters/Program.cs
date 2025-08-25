dd4a42e6a8349f77a2eb44afe2dc63d0560f8da9
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-25 12:02:34 +0330

    addDifferentTypeSyntax

--- Chapter5_ObjectOrientedProgramming/init-Only Setters/Program.cs
+++ Chapter5_ObjectOrientedProgramming/init-Only Setters/Program.cs
@@ -5,18 +5,35 @@ immutable هم میگویند.
  */
 
 
-User user = new User("Maryam", "Taghavi");
+// Default Constructor
+using System.Xml.Linq;
+
+User user = new User() { Name = "Maryam", Family = "Taghavi" };
+
+// If we have constructor
+//User user1 = new User("Maryam", "Taghavi");
+
 // compile error => init , readonly , immutable 
 //user.Name = "Parsa";
 
 class User
 {
+    // Automatic property syntax
+    public int Id { get; set; }
+
+    private int _age; // backing field
+
+    public int Age   // Traditional property syntax
+    {
+        get { return _age; }   
+        set { _age = value; }
+    }
     public string Name { get; init; }
     public string Family { get; init; }
 
-    public User(string Name , string Family)
-    {
-        this.Name = Name;
-        this.Family = Family;
-    }
+    //public User(string Name , string Family)
+    //{
+    //    this.Name = Name;
+    //    this.Family = Family;
+    //}
 }
