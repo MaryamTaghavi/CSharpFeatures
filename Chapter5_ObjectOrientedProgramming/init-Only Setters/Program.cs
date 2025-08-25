596973f941cedfc96cbd914fe29118a2316b774f
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-25 11:37:48 +0330

    addInit-Only

---
+++ Chapter5_ObjectOrientedProgramming/init-Only Setters/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+به هنگام ایجاد در کانستراکتور میتوانیم مقدار اولیه بدهیم ولی بعد از آن
+فیلد به readonly تبدیل میشود که به آن
+immutable هم میگویند.
+ */
+
+
+User user = new User("Maryam", "Taghavi");
+// compile error => init , readonly , immutable 
+//user.Name = "Parsa";
+
+class User
+{
+    public string Name { get; init; }
+    public string Family { get; init; }
+
+    public User(string Name , string Family)
+    {
+        this.Name = Name;
+        this.Family = Family;
+    }
+}
