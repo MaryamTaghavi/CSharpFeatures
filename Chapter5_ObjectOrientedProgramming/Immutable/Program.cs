ca157d9c3a9b2df196e921dc88687ccbbb2c26a5
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-25 13:47:20 +0330

    createImmutable

---
+++ Chapter5_ObjectOrientedProgramming/Immutable/Program.cs
@@ -0,0 +1,11 @@
+﻿// immutable va mmutable
+// immutable cannot change and they are read-only
+// they can set values in constructor
+// if we want to change value, create a new instance from that
+// مصرف حافظه immutable بیشتر از ئmmutable است 
+// بنابراین کندتر است
+// ولی thread-safe تر است. 
+// به این معنا که فرض کن دو تا ترد همزمان بخواهند متغیر را تغییر بدهند اگر immutable باشند
+// چون شی جدید میشازد پس تغییرات از بین نمیروند و در نتیجه race-condition وجود ندارد
+// اما اگر mmutable باشد و همزمان دو تا ترد بخواهند آن را تغییر دهند ممکن است race-condition به وجود بیاید.
+// موضوع تفاوت mmutable با immutable ربطی به referencetype و valuetype ندارد.
