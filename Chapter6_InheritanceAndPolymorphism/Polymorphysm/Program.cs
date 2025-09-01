a00502018abcb3a34264375a3c5f5e4750a2edd0
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-09-01 08:46:12 +0330

    createPolymorphism

---
+++ Chapter6_InheritanceAndPolymorphism/Polymorphysm/Program.cs
@@ -0,0 +1,41 @@
+﻿Animal animal = new Dog();
+animal.Speak(); // نتیجه: "Bark"
+
+Animal1 animal1 = new Dog1();
+animal1.Speak(); // نتیجه: "Animal sound"
+
+/*
+وقتی override و virtual را نداریم رفتار کلاس پدر نتیجه کار است
+اما وقتی میگذاریم رفتار کلاس فرزند نتیجه کار است
+ */
+public class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("Animal sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Animal1
+{
+    public void Speak()
+    {
+        Console.WriteLine("Animal sound");
+    }
+}
+
+public class Dog1 : Animal1
+{
+    public void Speak()
+    {
+        Console.WriteLine("Bark");
+    }
+}
