455cc056faa69eb36c1acec73f84126b918165e0
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-05 15:02:42 +0330

    createInteface

---
+++ Chapter8_WorkingWithInterfaces/Interface/InterfaceTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Interface;
+
+// از سی شارپ 8 به بعد اینترفیس میتواند هر جور دیتایی از قبیل متد با پیاده سازی، استاتیک، بدون پیاده سازی و ... را داشته باشد.
+public interface InterfaceTest
+{
+    public string Test()
+    {
+        return "";
+    }
+
+    public void Test1();
+
+    static string Test2 ;
+}
