a6d39c7b3aef7f43c5c9bf58460b1888763dcedd
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-05 17:18:32 +0330

    createIEnumeraor

--- Chapter8_WorkingWithInterfaces/IEnumrable/Program.cs
+++ Chapter8_WorkingWithInterfaces/IEnumrable/Program.cs
@@ -1,3 +1,17 @@
 ﻿// IEnumerable در واقع میگه که من یک مجموعه قابل پیمایش هستم و میتونی روی من foreach را اجرا کنی.
 
 // IEnumerator میگه: این Enumerator چطور حرکت کنه و آیتم فعلی چی باشه.
+
+// IEnumerable و IEnumerator باعث میشن هر چیزی شبیه Array بشه و قابل پیمایش باشه.
+
+public interface IEnumerator
+{
+    bool MoveNext();
+    object Current { get; }
+    void Reset();
+}
+
+public interface IEnumerable
+{
+    IEnumerator GetEnumerator();
+}   
