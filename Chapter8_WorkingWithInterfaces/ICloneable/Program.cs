9f1f980e5bd3bd35b6a4a5c8ab41bd6236124ef0
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-06 07:23:10 +0330

    addInterface

--- Chapter8_WorkingWithInterfaces/ICloneable/Program.cs
+++ Chapter8_WorkingWithInterfaces/ICloneable/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("***** Fun with Object Cloning *****\n");
+﻿// MemberwiseClone() => shallow copy
+
+Console.WriteLine("***** Fun with Object Cloning *****\n");
 // Two references to same object!
 Point p1 = new Point(50, 50);
 Point p2 = p1;
@@ -6,7 +8,20 @@ p2.X = 0;
 Console.WriteLine(p1);
 Console.WriteLine(p2);
 
-public class Point
+Point p3 = new Point(100, 100) ;
+Point p4 = (Point)p3.Clone() ;
+p4.X = 0;
+
+Console.WriteLine(p3);
+Console.WriteLine(p4);
+Console.ReadLine();
+
+public interface ICloneable
+{
+    object Clone();
+}
+
+public class Point : ICloneable
 {
     public int X { get; set; }
     public int Y { get; set; }
@@ -14,4 +29,5 @@ public class Point
     public Point() { }
     // Override Object.ToString().
     public override string ToString() => $"X = {X}; Y = {Y}";
+    public object Clone() => new Point(this.X, this.Y);
 }
