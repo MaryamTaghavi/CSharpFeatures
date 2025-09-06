9626f1f5f0c391addb178ffb5459d0cc0a267365
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-06 06:48:21 +0330

    addIEnumerableClass

--- Chapter8_WorkingWithInterfaces/YieldReturn/Program.cs
+++ Chapter8_WorkingWithInterfaces/YieldReturn/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+public class Car
+{
+    public string PetName { get; set; }
+    public int Speed { get; set; }
+
+    public Car(string name, int speed)
+    {
+        PetName = name;
+        Speed = speed;
+    }
+}
+
+public class Garage : IEnumerable
+{
+    private Car[] carArray = new Car[4];
+
+    public Garage()
+    {
+        carArray[0] = new Car("Rusty", 30);
+        carArray[1] = new Car("Clunker", 55);
+        carArray[2] = new Car("Zippy", 30);
+        carArray[3] = new Car("Fred", 30);
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        foreach (Car c in carArray)
+        {
+            yield return c;
+        }
+    }
+}
