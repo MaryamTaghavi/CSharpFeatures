d0f76934210aef6919e8f7721f7b4186a92251e2
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-07 22:50:19 +0330

    addDescriptionToGCTypes

--- ObjectLifeTime/GarbageCollectionTypes/Program.cs
+++ ObjectLifeTime/GarbageCollectionTypes/Program.cs
@@ -1,7 +1,10 @@
 ﻿/*
 
 Workstation garbage collection : مناسب اپلیکیشن‌های دسکتاپ، سریع و کم‌توقف.
+به دو صورت background و nonconcurrent انجام مشود.
 
+background gc : از .net4 به بعد آمد که به معنای جمع آوری اشیا روی heap بدون توقف کردن تمامی thread ها
+بعد از .Net 4  بهبود به این سمت بود که زمان توقف تردها پایین آورده شد .
 Server garbage collection : طراحی شده برای سرور‌ها و برنامه‌های High Throughput (مثل وب سرورها، سرویس‌های backend).
 
 */
