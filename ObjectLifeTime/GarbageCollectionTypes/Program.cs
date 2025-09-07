6744c74edad196c4b142d7571ada48ff0af83375
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-07 20:54:50 +0330

    addGarbageCollection

---
+++ ObjectLifeTime/GarbageCollectionTypes/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+
+Workstation garbage collection : مناسب اپلیکیشن‌های دسکتاپ، سریع و کم‌توقف.
+
+Server garbage collection : طراحی شده برای سرور‌ها و برنامه‌های High Throughput (مثل وب سرورها، سرویس‌های backend).
+
+*/
