939f7da6da29fae91320cafe17e62fb067f0ec59
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-07 20:22:55 +0330

    createObjectGeneration

--- ObjectLifeTime/NewKeyword/Program.cs
+++ ObjectLifeTime/NewKeyword/Program.cs
@@ -1,3 +1,6 @@
-﻿// New keyword returns reference to the object on the heap not the actual object.
+﻿
+// New keyword returns reference to the object on the heap not the actual object.
+
+// Reference to object is in stack
 
 // When you want to invoke members on the object, apply the C# dot operator to the stored reference.
