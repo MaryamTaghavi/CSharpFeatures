44a54c55fa9ce6cf47a8a2fca6cb087a8b29f619
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-07 17:14:43 +0330

    addDescriptionForNewKeyword

---
+++ ObjectLifeTime/NewKeyword/Program.cs
@@ -0,0 +1,3 @@
+﻿// New keyword returns reference to the object on the heap not the actual object.
+
+// When you want to invoke members on the object, apply the C# dot operator to the stored reference.
