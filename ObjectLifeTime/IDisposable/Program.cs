e3fa18e99d0b8bfad92312ee60736b3316a5d6b8
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-08 10:28:07 +0330

    completeDisposableProject

---
+++ ObjectLifeTime/IDisposable/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("***** Fun with Dispose *****\n");
+MyResourceWrapper rw = new MyResourceWrapper();
+if (rw is IDisposable)
+{
+    rw.Dispose();
+}
+Console.ReadLine();
+
+class MyResourceWrapper : IDisposable
+{
+    // The object user should call this method
+    // when they finish with the object.
+    public void Dispose()
+    {
+        // Clean up unmanaged resources...
+        // Dispose other contained disposable objects...
+
+        // Just for a test.
+        Console.WriteLine("***** In Dispose! *****");
+    }
+}
