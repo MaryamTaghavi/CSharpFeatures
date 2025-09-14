e56a61f6cf3c47b68f386caa04c8140376789a15
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-14 17:17:29 +0330

    completeExecutionContext

---
+++ Chapter15_Multithreaded/ExcetionContext/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using System;
+
+namespace ExcetionContext.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        /// <summary>
+        /// Execution Context
+        /// </summary>
+        /// <remarks>
+        /// Execution Context محیطی است که اطلاعات مرتبط با اجرای یک Thread را نگه می‌دارد.
+
+        // شامل چیزهایی مثل:
+
+//Security context(مجوزها)
+
+//Culture info(تنظیمات زبانی و منطقه‌ای)
+
+//Call context(اطلاعاتی که بین متدها منتقل می‌شود)
+
+//به عبارتی، Execution Context مثل کیسه‌ای از اطلاعات اجرایی Thread است که.NET آن را مدیریت می‌کند.
+        /// ## مراحل اعتبار سنجی
+        /// ***
+        /// * وارد کردن شماره موبایل یا ایمیل کاربر
+        /// * وارد کردن کد ارسالی به موبایل یا ایمیل
+        /// </remarks>
+        /// 
+
+        [HttpGet]
+        public void Get()
+        {
+           
+        }
+    }
+}
---
+++ Chapter15_Multithreaded/ExecutionContext/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+
+Execution Context چیست؟
+
+Execution Context محیطی است که اطلاعات مرتبط با اجرای یک Thread را نگه می‌دارد.
+
+شامل چیزهایی مثل:
+
+Security context (مجوزها)
+
+Culture info (تنظیمات زبانی و منطقه‌ای)
+
+Call context (اطلاعاتی که بین متدها منتقل می‌شود)
+
+به عبارتی، Execution Context مثل کیسه‌ای از اطلاعات اجرایی Thread است که .NET آن را مدیریت می‌کند.
+
+*/
+
+/*
+ 
+Execution Context محیطی است که اطلاعات مرتبط با اجرای یک Thread را نگه می‌دارد.
+
+شامل چیزهایی مثل:
+
+Security context (مجوزها)
+
+Culture info (تنظیمات زبانی و منطقه‌ای)
+
+Call context (اطلاعاتی که بین متدها منتقل می‌شود)
+
+به عبارتی، Execution Context مثل کیسه‌ای از اطلاعات اجرایی Thread است که .NET آن را مدیریت می‌کند.
+
+*/
+
+/*
+
+اگر از Async/Await یا Task استفاده کنید، Threadها ممکن است بین Contextهای مختلف جابه‌جا شوند و Execution Context اطمینان می‌دهد که اطلاعات مهم مثل Culture یا Security حفظ شوند.
+
+*/
