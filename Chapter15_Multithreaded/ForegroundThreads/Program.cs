d2558b117399c3977343dce529a40fed444f8b7b
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-15 12:56:43 +0330

    createForegroundThreads

---
+++ Chapter15_Multithreaded/ForegroundThreads/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+  
+ 1- برنامه تا پایان همه Foreground Threads بسته نمی‌شود
+ 2- کار اصلی برنامه، عملیات حیاتی
+ 3- هر Thread جدیدی که با Thread.Start() ایجاد می‌شود، Foreground است
+ 4- RunTime صبر می‌کند تا تمام Foreground Threads تمام شوند
+
+ */
