017054ad16ca1d22257648556ec40bc73f6125a8
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-15 10:52:42 +0330

    addMultiThreadMangement

---
+++ Chapter15_Multithreaded/MultiThreadMangement/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+ 
+برای مدیریت تردها چند راهکار وجود دارد :
+
+ 1- System.Threading namespace → شامل Thread، Monitor، Semaphore و…
+
+ 2- Task Parallel Library (TPL) → کار با Taskها به جای Threadهای خام
+
+ 3- async / await → نوشتن کد غیرهمزمان بدون پیچیدگی Thread management
+
+*/
