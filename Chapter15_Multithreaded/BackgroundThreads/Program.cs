8a99146241c87cb1a62357a70ceed45857c15340
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-15 12:54:40 +0330

    createBackgroundThreads

---
+++ Chapter15_Multithreaded/BackgroundThreads/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+
+1- گر همه Foreground Threads تمام شوند، Background Threads ناگهان متوقف می‌شوند.
+2- کاربرد : عملیات جانبی یا کم‌اهمیت، مثل لاگ گرفتن یا کارهای پس‌زمینه
+3- با ست کردن thread.IsBackground = true می‌توان آن را Background کرد.
+4- Background Threads بدون هشدار خاتمه داده می‌شوند
+
+*/
