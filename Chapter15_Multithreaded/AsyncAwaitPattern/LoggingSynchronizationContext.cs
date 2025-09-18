b3ae2d0267ac94ffcf958ba563e8d246dbd08c9b
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-18 21:06:54 +0330

    completeAsyncAwaitPattern

---
+++ Chapter15_Multithreaded/AsyncAwaitPattern/LoggingSynchronizationContext.cs
@@ -0,0 +1,10 @@
+﻿namespace AsyncAwaitPattern;
+
+class LoggingSynchronizationContext : SynchronizationContext
+{
+    public override void Post(SendOrPostCallback d, object state)
+    {
+        Console.WriteLine("کار به صف اضافه شد!");
+        ThreadPool.QueueUserWorkItem(_ => d(state));
+    }
+}
---
+++ Chapter15_Multithreaded/AsyncAwaitPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿// SynchronizationContext  یک کلاس پایه در .Net است
+// که سبب میشود به ترد مربوطه بگوییم که ادامه کار پس از پایان یافتن تسک روی چه تردی انجام شود
+// بدون آن به انتخاب خود .Net این کار انجام میشود.
+// تنظیمات آن معمولا به دست ما نیست و خود برنامه بر اساس نوع پروژه آن را تنظیم میکند.
+
+using AsyncAwaitPattern;
+
+static async Task Main()
+{
+    SynchronizationContext.SetSynchronizationContext(new LoggingSynchronizationContext());
+
+    Console.WriteLine("قبل از await");
+
+    await Task.Delay(1000);
+
+    Console.WriteLine("بعد از await");
+}
