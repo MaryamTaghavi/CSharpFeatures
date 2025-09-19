730e23ee6e636a5ba65c9394b7f0f8c70d89645b
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-19 18:06:52 +0330

    addEagerLoadingWithFilter

--- Chapter22_ExploringEntityFrameworkCore/EagerLoading/Program.cs
+++ Chapter22_ExploringEntityFrameworkCore/EagerLoading/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+Eager Loading به معنای بارگذاری فوری داده‌های مرتبط (related entities) از چند جدول در یک فراخوانی پایگاه داده است.
+
+معادلش در SQL: یک query با JOIN بین دو یا چند جدول.
+
+وقتی موجودیت‌ها (Entities) Navigation Property دارند و در LINQ query استفاده می‌شوند، EF Core از JOIN برای بارگذاری داده‌های مرتبط استفاده می‌کند.
+
+مزیت: کارایی بالاتر، چون به جای اجرای چند query جداگانه (یک query برای هر جدول مرتبط)، همه داده‌ها در یک query دریافت می‌شوند. 
+
+*/
+
+// Eagerloading with filter
+
+var context = new Object() ;
+
+var makesWithYellowCars = context.Makes
+.Include(x => x.Cars.Where(x => x.Color == "Yellow"))
+.ToList();
