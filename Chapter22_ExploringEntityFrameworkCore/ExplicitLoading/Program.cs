4d82a2c97d575378923fdbf760eb8d11de5ca9e4
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-19 18:57:39 +0330

    createExplicitLoading

---
+++ Chapter22_ExploringEntityFrameworkCore/ExplicitLoading/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+
+ Explicit Loading یعنی بارگذاری داده‌های مرتبط (related entities) بعد از اینکه شیء اصلی (core entity) قبلاً در حافظه بارگذاری شده است.
+
+*/
+
+// بارگذاری شیء اصلی
+var car = context.Cars.FirstOrDefault(c => c.Id == 1);
+
+// بارگذاری یک Navigation Property تک‌تایی
+context.Entry(car).Reference(c => c.Make).Load();
+
+// بارگذاری یک Navigation Property مجموعه‌ای
+context.Entry(car).Collection(c => c.Drivers)
+    .Query() // می‌توان شروط اضافه کرد
+    .Where(d => d.Age > 25)
+    .Load();
+
+// با reference یا collection دیتا را بدست می آورد و
+// در صورت زدن .ToList()  و .Load()  کوئری اجرا میشود.
