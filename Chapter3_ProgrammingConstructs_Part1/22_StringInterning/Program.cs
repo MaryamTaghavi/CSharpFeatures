b0d66db78896ea05863183414501e6bfab60c98e
MaryamTaghavi <75110582+MaryamTaghavi@users.noreply.github.com>
2025-10-03 23:46:55 +0330

    Update Program.cs

--- Chapter3_ProgrammingConstructs_Part1/22_StringInterning/Program.cs
+++ Chapter3_ProgrammingConstructs_Part1/22_StringInterning/Program.cs
@@ -1,1 +1,7 @@
+ وقتی دو رشته یکسان داشته باشیم، هر دو به یک خانه از حافظه اشاره میکنند
+ این ویژگی سبب میشود که حافظه کمتری مصرف شود و رشته ها سریعتر با هم مقایسه شوند.
 
+string a = "hello";
+string b = "hello";
+
+Console.WriteLine(Object.ReferenceEquals(a, b)); // true → هر دو interned
